dfb18cf8a964ad00148362f80e48d651d9b28a19
William Yang <wy2037@nyu.edu>
2023-02-07 13:32:52 -0500

    Weapon Functionality Pt 3

--- Prototype 2- Game Feel/Assets/Hit.cs
+++ Prototype 2- Game Feel/Assets/Hit.cs
@@ -9,8 +9,8 @@ public class Hit : MonoBehaviour
     [SerializeField]
     private float xRange, yRange;
     private float cooldown = 0f;
-    public GameObject blade;
-    public int weapon;
+    public GameObject blade, bomb;
+    public int weapon, bombCount = 0;
     public Health hp;
 
     void Start() {
@@ -20,12 +20,14 @@ public class Hit : MonoBehaviour
     void Update() {
         if (weapon == 3) {
             blade.SetActive(true);
-        } else if (weapon == 3) {
+        } else if (weapon == 4) {
             blade.SetActive(false);
+                if(Input.GetMouseButtonDown(0) && bombCount < 3) {
+                    bombCount += 1;
+                }
         } else {
             blade.SetActive(false);
         }
-
         if (cooldown > 0) {
             cooldown -= Time.deltaTime;
         }
