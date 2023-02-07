9a39e5c1e2600a873144ca7b6d35f5806cf85a81
William Yang <wy2037@nyu.edu>
2023-02-06 21:53:53 -0500

    Some more scripts pt 2

--- Prototype 2- Game Feel/Assets/Hit.cs
+++ Prototype 2- Game Feel/Assets/Hit.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Hit : MonoBehaviour
 {
+    [SerializeField]
+    Rigidbody2D rb;
+    [SerializeField]
+    private float xRange, yRange;
+
     // Update is called once per frame
     void OnMouseDown() 
     {
-        Debug.Log("test");
+        rb.AddForce(new Vector3(Random.Range(xRange * -1, xRange), Random.Range(yRange/3, yRange)));
     }
 }
